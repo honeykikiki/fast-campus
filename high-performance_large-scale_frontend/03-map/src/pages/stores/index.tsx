5c16cd0afb9a8c83e7b301b139b817828f11df4f
허성진 <tjdwls75666@naver.com>
2024-03-15 01:12:19 +0900

    intersectionObsever기능 추가 useIntersectionObsever 기능 추가

--- high-performance_large-scale_frontend/03-map/src/pages/stores/index.tsx
+++ high-performance_large-scale_frontend/03-map/src/pages/stores/index.tsx
@@ -1,25 +1,70 @@
-import Loading from '@/components/Loading';
-import Pagination from '@/components/Pagination';
-import { StoreApiResponse } from '@/interface';
+import React, { useCallback, useEffect, useRef } from 'react';
+
 import axios from 'axios';
 import Image from 'next/image';
-import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { useQuery } from 'react-query';
+import { useInfiniteQuery } from 'react-query';
 
-const URL_PATH = '/stores';
+import Loading from '@/components/Loading';
+import Pagination from '@/components/Pagination';
+import { StoreApiResponse, StoreType } from '@/interface';
+import useIntersectionObserver from '@/hooks/useIntersectionObserver';
+import Loader from '@/components/Loader';
 
 export default function StoreListPage() {
   const router = useRouter();
   const { page = '1' }: any = router.query;
+  const ref = useRef<HTMLDivElement | null>(null);
+  const pageRef = useIntersectionObserver(ref, {});
+  const isPageEnd = !!pageRef?.isIntersecting;
+
+  // const {
+  //   isLoading,
+  //   isError,
+  //   data: stores,
+  // } = useQuery('stores', async () => {
+  //   const { data } = await axios(`/api/stores?page=${page}`);
+  //   return data as StoreApiResponse;
+  // });
+
+  const fetchStores = async ({ pageParam = 1 }) => {
+    const { data } = await axios('/api/stores?page=' + pageParam, {
+      params: {
+        limit: 10,
+        page: pageParam,
+      },
+    });
+
+    return data;
+  };
+
   const {
+    data: stores,
+    isFetching,
+    fetchNextPage,
+    isFetchingNextPage,
+    hasNextPage,
     isLoading,
     isError,
-    data: stores,
-  } = useQuery('stores', async () => {
-    const { data } = await axios(`/api/stores?page=${page}`);
-    return data as StoreApiResponse;
+  } = useInfiniteQuery('stores', fetchStores, {
+    getNextPageParam: (lastPage) =>
+      lastPage.data.length > 0 ? lastPage.page + 1 : undefined,
   });
+  const FetchNext = useCallback(async () => {
+    const res = await fetchNextPage();
+    if (res.isError) {
+      console.log(res.error);
+    }
+  }, [fetchNextPage]);
+
+  useEffect(() => {
+    let timerId: NodeJS.Timeout | undefined;
+    if (isPageEnd && hasNextPage) {
+      timerId = setTimeout(() => fetchNextPage(), 500);
+    }
+
+    return () => clearTimeout(timerId);
+  }, [FetchNext, isPageEnd, hasNextPage]);
 
   if (isError) {
     return (
@@ -35,47 +80,52 @@ export default function StoreListPage() {
         {isLoading ? (
           <Loading />
         ) : (
-          stores?.data?.map((store, index) => (
-            <li className="flex justify-between gap-x-6 py-5" key={store.id}>
-              <div className="flex gap-x-4">
-                <Image
-                  src={
-                    store?.category
-                      ? `/images/markers/${store?.category}.png`
-                      : '/images/markers/default.png'
-                  }
-                  width={48}
-                  height={48}
-                  alt={'이미지'}
-                />
-                <div className="">
-                  <div className="text-sm font-semibold leading-9 text-gray-900">
-                    {store?.name}
+          stores?.pages?.map((page, index) => (
+            <React.Fragment key={index}>
+              {page.data.map((store: StoreType, i: number) => (
+                <li
+                  className="flex justify-between gap-x-6 py-5"
+                  key={store.id}
+                >
+                  <div className="flex gap-x-4">
+                    <Image
+                      src={
+                        store?.category
+                          ? `/images/markers/${store?.category}.png`
+                          : '/images/markers/default.png'
+                      }
+                      width={48}
+                      height={48}
+                      alt={'이미지'}
+                    />
+                    <div className="">
+                      <div className="text-sm font-semibold leading-9 text-gray-900">
+                        {store?.name}
+                      </div>
+                      <div className="mt-1 text-xs font-semibold leading-5 text-gray-700">
+                        {store?.name}
+                      </div>
+                    </div>
                   </div>
-                  <div className="mt-1 text-xs font-semibold leading-5 text-gray-700">
-                    {store?.name}
+                  <div className="hidden sm:flex sm:flex-col sm:items-end">
+                    <div className="text-sm font-semibold leading-9 text-gray-900">
+                      {store?.address}
+                    </div>
+                    <div className="mt-1 text-xs font-semibold leading-5 text-gray-700">
+                      {store?.phone || '번호 없음'} | {store?.foodCertifyName} |{' '}
+                      {store?.category}
+                    </div>
                   </div>
-                </div>
-              </div>
-              <div className="hidden sm:flex sm:flex-col sm:items-end">
-                <div className="text-sm font-semibold leading-9 text-gray-900">
-                  {store?.address}
-                </div>
-                <div className="mt-1 text-xs font-semibold leading-5 text-gray-700">
-                  {store?.phone || '번호 없음'} | {store?.foodCertifyName} |{' '}
-                  {store?.category}
-                </div>
-              </div>
-            </li>
+                </li>
+              ))}
+            </React.Fragment>
           ))
         )}
       </ul>
 
-      <Pagination
-        total={stores?.totalPage ?? 0}
-        page={page}
-        urlPath={URL_PATH}
-      />
+      {(isFetching || hasNextPage || isFetchingNextPage) && <Loader />}
+
+      <div ref={ref} className="w-full touch-none h-10 mb-10"></div>
     </div>
   );
 }
