947eb4aa4636fb3cb381074fb7bf70aaa337f346
허성진 <tjdwls75666@naver.com>
2024-04-09 18:33:46 +0900

    회원가입 Form 추가

--- high-performance_large-scale_frontend/05-mcard/src/components/singnup/Form.tsx
+++ high-performance_large-scale_frontend/05-mcard/src/components/singnup/Form.tsx
@@ -3,19 +3,100 @@ import Flex from '@shared/Flex'
 import { Spacing } from '@shared/Spacing'
 import TextFiled from '@shared/TextField'
 import { css } from '@emotion/react'
+import { ChangeEvent, useCallback, useMemo, useState } from 'react'
+import validator from 'validator'
+import { FormValues } from '@/models/signup'
 
-function Form() {
+function Form({ onSubmit }: { onSubmit: (formValues: FormValues) => void }) {
+  const [formValues, setFormValues] = useState<FormValues>({
+    email: '',
+    password: '',
+    rePassword: '',
+    name: '',
+  })
+  const [dirty, setDirty] = useState<Partial<FormValues>>({})
+
+  const handleFormValues = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { name, value },
+    } = e
+
+    setFormValues((prevFormValues) => ({
+      ...prevFormValues,
+      [name]: value,
+    }))
+  }, [])
+
+  const handleBlur = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { name },
+    } = e
+
+    setDirty((prev) => ({
+      ...prev,
+      [name]: 'true',
+    }))
+  }, [])
+
+  const errors = useMemo(() => validate(formValues), [formValues])
+
+  const 제출가능한상태인가 = Object.keys(errors).length === 0
+
   return (
     <Flex direction="column" css={formContainerStyles}>
-      <TextFiled label="이메일" placeholder="이메일을 입력해주세요." />
+      <TextFiled
+        label="이메일"
+        name="email"
+        placeholder="이메일을 입력해주세요."
+        value={formValues.email}
+        onChange={handleFormValues}
+        hasError={Boolean(errors.email) && Boolean(dirty.email)}
+        helpMessage={Boolean(dirty.email) ? errors.email : ''}
+        onBlur={handleBlur}
+      />
       <Spacing size={16} />
-      <TextFiled label="패스워드" type="password" />
+      <TextFiled
+        label="패스워드"
+        name="password"
+        type="password"
+        placeholder="비밀번호를 입력해주세요."
+        value={formValues.password}
+        onChange={handleFormValues}
+        hasError={Boolean(errors.password) && Boolean(dirty.password)}
+        helpMessage={Boolean(dirty.password) ? errors.password : ''}
+        onBlur={handleBlur}
+      />
       <Spacing size={16} />
-      <TextFiled label="패스워드 재확인" type="password" />
+      <TextFiled
+        label="패스워드 재확인"
+        name="rePassword"
+        type="password"
+        placeholder="비밀번호를 재 입력해주세요."
+        value={formValues.rePassword}
+        onChange={handleFormValues}
+        hasError={Boolean(errors.rePassword) && Boolean(dirty.rePassword)}
+        helpMessage={Boolean(dirty.rePassword) ? errors.rePassword : ''}
+        onBlur={handleBlur}
+      />
       <Spacing size={16} />
-      <TextFiled label="이름" placeholder="이름을 입력해주세요." />
+      <TextFiled
+        label="이름"
+        name="name"
+        placeholder="이름을 입력해주세요."
+        value={formValues.name}
+        onChange={handleFormValues}
+        hasError={Boolean(errors.name) && Boolean(dirty.name)}
+        helpMessage={Boolean(dirty.name) ? errors.name : ''}
+        onBlur={handleBlur}
+      />
 
-      <FixedBottomButton label="회원가입" onClick={() => {}} disabled={true} />
+      <FixedBottomButton
+        label="회원가입"
+        onClick={() => {
+          onSubmit(formValues)
+        }}
+        disabled={제출가능한상태인가 === false}
+      />
     </Flex>
   )
 }
@@ -23,5 +104,31 @@ function Form() {
 const formContainerStyles = css`
   padding: 24px;
 `
+
+function validate(formValues: FormValues) {
+  let errors: Partial<FormValues> = {}
+
+  if (validator.isEmail(formValues.email) === false) {
+    errors.email = '이메일 형식을 확인해주세요.'
+  }
+
+  if (formValues.password.length < 8) {
+    errors.password = '비밀번호를 8자 이상 입력해주세요.'
+  }
+
+  if (formValues.rePassword.length < 8) {
+    errors.rePassword = '비밀번호를 8글자 이상 입력해주세요'
+  } else if (
+    validator.equals(formValues.rePassword, formValues.password) === false
+  ) {
+    errors.rePassword = '비밀번호를 확인해주세요'
+  }
+
+  if (formValues.name.length < 2) {
+    errors.name = '이름은 2글자 이상 입력해주세요.'
+  }
+
+  return errors
+}
 
 export default Form
--- high-performance_large-scale_frontend/05-mcard/src/pages/Signup.tsx
+++ high-performance_large-scale_frontend/05-mcard/src/pages/Signup.tsx
@@ -1,9 +1,33 @@
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
+import { collection, doc, setDoc } from 'firebase/firestore'
 import Form from '@/components/singnup/Form'
+import { COLLECTIONS } from '@/constants'
+import { FormValues } from '@/models/signup'
+import { auth, store } from '@/remote/firebase'
 
 function Signup() {
+  const handleSubmit = async (formValues: FormValues) => {
+    const { email, password, name } = formValues
+
+    const { user } = await createUserWithEmailAndPassword(auth, email, password)
+
+    updateProfile(user, {
+      displayName: name,
+    })
+
+    const newUser = {
+      uid: user.uid,
+      email: user.email,
+      displayName: name,
+    }
+
+    await setDoc(doc(collection(store, COLLECTIONS.USER), user.uid), newUser)
+    console.log(user)
+  }
+
   return (
     <div>
-      <Form />
+      <Form onSubmit={handleSubmit} />
     </div>
   )
 }
