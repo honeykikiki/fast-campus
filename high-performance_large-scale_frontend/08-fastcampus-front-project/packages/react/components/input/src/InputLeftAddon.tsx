a3e888042efcf33eece82d429ae91bd986d3dd9d
허성진 <tjdwls75666@naver.com>
2024-06-25 01:21:30 +0900

    InputLeftAddon 컴포넌트 추가

---
+++ high-performance_large-scale_frontend/08-fastcampus-front-project/packages/react/components/input/src/InputLeftAddon.tsx
@@ -0,0 +1,27 @@
+import { clsx } from "clsx";
+import { InputLeftAddonProps } from "./types";
+import { inputLeftAddonStyle } from "./style.css";
+import { vars } from "@fastcampus/themes";
+import * as React from "react";
+
+const InputLeftAddon = (props: InputLeftAddonProps) => {
+  const { size = "md", color = "gray", children } = props;
+
+  return (
+    <div
+      className={clsx([
+        inputLeftAddonStyle({
+          size,
+        }),
+      ])}
+      style={{
+        color: vars.colors.$scale[color][900],
+      }}
+    >
+      {children}
+    </div>
+  );
+};
+
+InputLeftAddon.displayName = "InputLeftAddon";
+export { InputLeftAddon };
